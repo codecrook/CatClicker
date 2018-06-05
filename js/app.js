b625b2f182c7f2b34a3d218e49602af4ce28fed4
codecrook <hello@codecrook.io>
2018-06-05 20:24:56 +0530

    Binded data to HTML of app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var ViewModel = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable(img/22252709_010df3379e_z.jpg);
+  this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
 
   this.increamentCounter = function() {
     this.clickCount(this.clickCount() + 1);
