6c68967ebd72d0ba00037b50f21882b8d5dbb89b
codecrook <hello@codecrook.io>
2018-06-06 00:24:07 +0530

    added multiple cats

--- js/app.js
+++ js/app.js
@@ -1,7 +1,42 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+var initialCats =[
+  {
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    catNickNames: ['Tab', 'Furry', 'Puffy', 'Meowwy']
+  },
+  {
+    clickCount: 0,
+    name: 'Toffy',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    catNickNames: ['Tuff', 'Trr']
+  },
+  {
+    clickCount: 0,
+    name: 'Trikky',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    catNickNames: ['Trek', 'Trik']
+  },
+  {
+    clickCount: 0,
+    name: 'Taddy',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    catNickNames: ['Tad', 'Ted']
+  },
+  {
+    clickCount: 0,
+    name: 'Tulky',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    catNickNames: ['Talk', 'Trll']
+  }
+]
+
+
+var Cat = function(catData) {
+  this.clickCount = ko.observable(catData.clickCount);
+  this.name = ko.observable(catData.name);
+  this.imgSrc = ko.observable(catData.imgSrc);
+  this.catNickNames = ko.observableArray(catData.catNickNames);
   this.catLables = ko.computed(function() {
     if (this.clickCount() < 10) {
       return 'Newborn';
@@ -12,15 +47,25 @@ var Cat = function() {
     }
   }, this);
 
-  this.catNickNames = ko.observableArray(['Furry', 'Puffy', 'Meowwy']);
+
 }
 
 var ViewModel = function() {
   var self = this;
-  this.currentCat = ko.observable(new Cat());
+
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem));
+  });
+  this.currentCat = ko.observable(this.catList()[0]);
 
   this.increamentCounter = function() {
     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  }
+
+  this.setCat = function(clickedCat) {
+    self.currentCat(clickedCat);
   }
 }
 ko.applyBindings(new ViewModel());
