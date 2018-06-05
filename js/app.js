b15b2dd4e63ad6a764d34a8b30e248646ad91b9f
codecrook <hello@codecrook.io>
2018-06-05 21:09:22 +0530

    separated Model from ViewModel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ViewModel = function() {
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
@@ -13,9 +13,13 @@ var ViewModel = function() {
   }, this);
 
   this.catNickNames = ko.observableArray(['Furry', 'Puffy', 'Meowwy']);
+}
+
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Cat());
 
   this.increamentCounter = function() {
-    this.clickCount(this.clickCount() + 1);
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   }
 }
 ko.applyBindings(new ViewModel());
